23296e3d482199cd8795110b4537721111fdbac3
thunderxx2 <devdushyantatwork@gmail.com>
2024-01-16 23:10:02 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// script.js
+// Function to fetch and include HTML content
+function includeHTML(file, containerId){
+    fetch(file)
+    .then(response => response.text())
+    .then(
+        html => {
+            document.getElementById(containerId).innerHTML = html;
+        }
+    )
+}
+
+// Include content from about.html
+includeHTML('about/about.html', 'aboutMeContainer');
+
+// Include content from contact.html
+includeHTML('contact/contact.html', 'contactMeContainer');
